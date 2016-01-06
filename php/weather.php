16307b53e961e6a2ff33497de94997765389c4df
David Han <dhansolo@uw.edu>
2016-01-06 11:25:58 -0800

    Added feature to allow query strings...kind of

--- php/weather.php
+++ php/weather.php
@@ -1,8 +1,11 @@
 <?php
-$url = parse_url($_SERVER['REQUEST_URI']);
-$url = 'http://api.openweathermap.org/data/2.5/weather?q=Seattle,us&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0';
+$requesturl = parse_url($_SERVER['REQUEST_URI']);
+$city = substr($requesturl['path'], 1);
+if(strlen($city) == 0) {
+    $city = 'seattle';
+}
+$url = 'http://api.openweathermap.org/data/2.5/weather?q=' . $city . ',us&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0';
 $result = json_decode(file_get_contents($url), true);
-$name = substr($url['path'], 1);
 echo '<pre>';
 print_r($result);
 ?>
